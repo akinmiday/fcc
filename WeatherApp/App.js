50b0a6b0e73b2c0ce39e1c272246e7503ce550e9
akinmiday <110313060+akinmiday@users.noreply.github.com>
2023-07-21 21:46:46 +0100

    fetch API started

--- WeatherApp/App.js
+++ WeatherApp/App.js
@@ -3,14 +3,30 @@ import { ActivityIndicator, View, StyleSheet } from "react-native"
 import Tabs from "./src/components/Tabs"
 import { NavigationContainer } from "@react-navigation/native"
 import * as Location from "expo-location"
-// import { TEST_KEY } from "@env"
-
-//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+import { WEATHER_API_KEY } from "./api"
 
 const App = () => {
   const [loading, setLoading] = useState(true)
   const [location, setLocation] = useState(null)
   const [error, setError] = useState(null)
+  const [weather, setWeather] = useState([])
+  const [lat, setLat] = useState([])
+  const [lon, setLon] = useState([])
+
+  const fetchWeatheData = async () => {
+    try {
+      const res = await fetch(
+        `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+      )
+      const data = await res.json()
+      setWeather(data)
+      setLoading(false)
+    } catch (e) {
+      setError("Could not fetch weather")
+    } finally {
+      setLoading(false)
+    }
+  }
 
   useEffect(() => {
     ;(async function () {
@@ -20,21 +36,24 @@ const App = () => {
         return
       }
       let location = await Location.getCurrentPositionAsync({})
-      setLocation(location)
+      setLat(location.coords.latitude)
+      setLon(location.coords.longitude)
+
+      await fetchWeatheData()
     })
-  }, [])
+  }, [lat, lon])
 
-  if (location) {
-    console.log(location)
+  if (weather) {
+    console.log()
   }
 
-  if (loading) {
-    return (
-      <View style={styles.container}>
-        <ActivityIndicator size={"large"} color={"blue"} />
-      </View>
-    )
-  }
+  // if (loading) {
+  //   return (
+  //     <View style={styles.container}>
+  //       <ActivityIndicator size={"large"} color={"blue"} />
+  //     </View>
+  //   )
+  // }
   return (
     <NavigationContainer>
       <Tabs />
